adb1993030edb22885cebe006a817cf3cf381da4
gyrodope69 <86244576+gyrodope69@users.noreply.github.com>
2022-05-24 11:38:44 +0530

    Product of array except self

---
+++ Product_of_array_except_self.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+ vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> left(n);
+        vector<int> right(n);
+        vector<int> ans(n);
+        
+        left[0]=1;
+        right[n-1]=1;
+        for(int i = 1 ; i<n ; i++)
+        {
+            left[i]=nums[i-1]*left[i-1];
+        }
+        
+        for(int i=n-2 ; i>=0 ; i--)
+        {
+          right[i]=nums[i+1]*right[i+1];
+        }
+        
+        for(int i = 0 ; i<n ; i++)
+        {
+          ans[i]=left[i]*right[i];
+        }
+        
+        return ans;
+ }
+int main()
+{
+    int n;
+    cout<<"Enter the number of elements :";
+    cin>>n;
+    vector<int> nums(n);
+    for(int i=0; i<n; i++)
+     cin>>nums[i];
+    
+    vector<int> ans = productExceptSelf(nums);
+    cout<<"Array is : ";
+    for(auto i:ans) 
+      {
+          cout<<i<<" ";
+      }
+    return 0;
+
+}
