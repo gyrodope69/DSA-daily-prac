7fa4490fe410b9e002bd32e074732fa015684859
gyrodope69 <royr91430@gmail.com>
2022-12-12 22:00:34 -0600

    .\climbing_stairs.cpp

---
+++ climbing_stairs.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+
+    public:
+    int climbingStairs(int n)
+    {
+        vector<int> dp(n+2,0);
+        dp[0]=1;
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3; i<=n; i++)
+        {
+          dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+};
+
+int main()
+{
+    Solution ob;
+    int n;
+    cin>>n;
+    cout<<ob.climbingStairs(n)<<endl;
+}
